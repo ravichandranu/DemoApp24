09d2aed2dcf84a6c1fc2087ac528859baa9321d2
ravichandranu <root.ravi92@gmail.com>
2023-11-23 02:11:56 -0800

    First Commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number 1");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Number 2");
+            int number2 = int.Parse(Console.ReadLine());
+            int addresult = number1 + number2;
+            int diffresult= Math.Abs(number1 - number2);
+            int prodresult = number1 * number2;
+            Console.WriteLine("Addition of Two number is " + addresult);
+            Console.WriteLine("Sum of {0} and {1} is {2}", number1, number2, addresult);
+            Console.WriteLine("difference between {0} and {1} is {2}", number1, number2, diffresult);
+            Console.WriteLine("Product of {0} and {1} is {2}", number1, number2, prodresult);
+
+
+
+           
+
+
+
+
+        }
+    }
+}
